8eda0632ccd3f4b28579788440f3ba4bca079c97
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-04-01 14:49:03 -0300

    create: overview for movie

--- lib/app/modules/description/description_page.dart
+++ lib/app/modules/description/description_page.dart
@@ -84,6 +84,13 @@ class _DescriptionPageState extends State<DescriptionPage> {
                 height: 1,
                 color: Colors.grey,
               ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Text(
+                  "Simba idolizes his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub's arrival. Scar, Mufasa's brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba's exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
             ],
           ),
         ),
