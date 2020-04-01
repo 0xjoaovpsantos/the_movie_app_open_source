07cb1f4b577b92f0b8c36da6735c9a17614b4cbd
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-04-01 14:18:39 -0300

    create: backdrop for movie

--- lib/app/modules/description/description_page.dart
+++ lib/app/modules/description/description_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:the_movie_app_open_source/app/utils/app_colors.dart';
 
 class DescriptionPage extends StatefulWidget {
   final String title;
@@ -13,11 +14,65 @@ class _DescriptionPageState extends State<DescriptionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      backgroundColor: AppColors.backgroundDescription,
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 280,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "http://image.tmdb.org/t/p/original//3s9O5af2xWKWR5JzP2iJZpZeQQg.jpg"),
+                        fit: BoxFit.fill)),
+              ),
+              Container(
+                color: AppColors.posterMovie,
+                child: Container(
+                  margin: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            "Coco",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "    2017",
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            "109m | ",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          Text(
+                            "Animation, Adventure, Comedy",
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/app/utils/app_colors.dart
+++ lib/app/utils/app_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color greySearch = Color(0xffEEEEEE);
+  static Color posterMovie = Color(0xff262626);
+  static Color backgroundDescription = Color(0xff333333);
 }
