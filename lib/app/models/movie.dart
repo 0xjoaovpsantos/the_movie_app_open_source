a31f8ba3252b7e5d82819ad32dd32bb767499f7c
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-04-03 12:43:57 -0300

    create: model to movie

---
+++ lib/app/models/movie.dart
@@ -0,0 +1,24 @@
+class Movie {
+  int id;
+  String posterPath;
+  String title;
+  String releaseDate;
+
+  Movie({this.id, this.posterPath, this.title, this.releaseDate});
+
+  Movie.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    posterPath = json['poster_path'];
+    title = json['title'];
+    releaseDate = json['release_date'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['poster_path'] = this.posterPath;
+    data['title'] = this.title;
+    data['release_date'] = this.releaseDate;
+    return data;
+  }
+}
