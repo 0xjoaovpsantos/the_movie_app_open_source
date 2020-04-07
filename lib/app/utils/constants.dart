10616954f29afe32248c49da563e1c32047f8b9e
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-04-07 19:05:16 -0300

    fix: key api in constants.dart

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,3 +1,5 @@
+import 'package:the_movie_app_open_source/app/utils/key.dart';
+
 class Constants {
   static String baseUrlUpComingMovies =
       "https://api.themoviedb.org/3/movie/upcoming?api_key=";
@@ -6,5 +8,5 @@ class Constants {
   static String baseUrlDescriptionMovie = "https://api.themoviedb.org/3/movie/";
   static String baseUrlPosterMovie = "http://image.tmdb.org/t/p/w500/";
   static String baseUrlBackdropImg = "http://image.tmdb.org/t/p/w500/";
-  static String keyTheMovieAdb = "YOUR API KEY THE MOVIE ADB";
+  static String keyTheMovieAdb = Key.keyString;
 }
