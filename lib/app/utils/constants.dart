f7307249fdbad27142f770f7e3625bf15b191fd2
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-04-13 21:51:35 -0300

    fix: key the movie adb

--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,5 +1,3 @@
-import 'package:the_movie_app_open_source/app/utils/key.dart';
-
 class Constants {
   static String baseUrlUpComingMovies =
       "https://api.themoviedb.org/3/movie/upcoming?api_key=";
@@ -8,5 +6,5 @@ class Constants {
   static String baseUrlDescriptionMovie = "https://api.themoviedb.org/3/movie/";
   static String baseUrlPosterMovie = "http://image.tmdb.org/t/p/w500/";
   static String baseUrlBackdropImg = "http://image.tmdb.org/t/p/w500/";
-  static String keyTheMovieAdb = Key.keyString;
+  static String keyTheMovieAdb = "YOUR API KEY THE MOVIE ADB";
 }
